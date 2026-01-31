2ec6f0f1f534d04dde6d9a561455239cecbb502e
sultankakim27-lab <sultankakim27@gmail.com>
2026-01-31 20:14:27 +0500

    Add_Practice2_boolean_if_else_loops

--- Practice2/boolean/boolean_intro.py
+++ Practice2/boolean/boolean_intro.py
@@ -6,4 +6,4 @@ print("Logged in:", is_logged_in)
 print("Has subscription:", has_subscription)
 
 can_watch = is_logged_in and has_subscription
-print("Can watch content:", can_watch)
+print("Can watch content:", can_watch) 
